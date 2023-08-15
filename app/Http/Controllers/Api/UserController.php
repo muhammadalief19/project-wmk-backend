99d85271d1cc26a70d64cb36eec4b6be46e181d2
muhammmadalief19 <aliefmuhammad1908@gmail.com>
2023-08-15 20:23:02 +0700

    added method register, login and logout

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
 {
@@ -81,5 +85,99 @@ class UserController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+
+    // method register
+    public function register(Request $request) {
+        // set validation
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email:dns|unique:users',
+            'role' => 'required',
+            'password' => 'required|min:8|confirmed'
+        ],
+        [
+            'name.required' => 'nama wajib diisi',
+            'email.required' => 'email wajib diisi',
+            'email.email' => 'format email tidak sesuai',
+            'email.unique' => 'email tidak tersedia',
+            'role.required' => 'role wajib diisi',
+            'password.required' => 'password wajib diisi',
+            'password.min' => 'password minimal 8 karakter',
+            'password.confirmed' => 'password belum dikonfirmasi'
+        ]
+        );
+
+        // if falidation fails
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // create user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role' => $request->role,
+            'password' => Hash::make($request->password)
+        ]);
+
+        //return response JSON user is created
+        if($user) {
+            return response()->json([
+                'success' => true,
+                'user'    => $user,  
+            ], 201);
+        }
+
+        //return JSON process insert failed 
+        return response()->json([
+            'success' => false,
+        ], 409);
+
+    }
+
+    public function login(Request $request) {
+        // set validation
+        $validator = Validator::make($request->all(), [
+            'email'     => 'required',
+            'password'  => 'required'
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //get credentials from request
+        $credentials = $request->only('email', 'password');
+
+        //if auth failed
+        if(!$token = JWTAuth::attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Email atau Password Anda salah'
+            ], 401);
+        }
+
+        //if auth success
+        return response()->json([
+            'success' => true,
+            'user'    => auth()->user(),    
+            'token'   => $token   
+        ], 200);
+    }
+
+    public function logout() {
+        //remove token
+        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+
+        if($removeToken) {
+            //return response JSON
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Berhasil!',  
+            ]);
+        }
     }
 }
