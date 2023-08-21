5513d02248df96983251aca0a62a40d5769e4b15
muhammmadalief19 <aliefmuhammad1908@gmail.com>
2023-08-21 21:07:46 +0700

    updated field in table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->enum('role', ['admin', 'driver']);
+            $table->enum('verify', ['pending', 'verified']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
